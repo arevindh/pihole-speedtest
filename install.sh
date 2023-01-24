8e12e393581d536b845a0f27a063394d0216d732
ipitio <21136719+ipitio@users.noreply.github.com>
2023-01-23 22:59:00 -0500

    swap at the end

--- install.sh
+++ install.sh
@@ -1,6 +1,24 @@
 #!/bin/bash -e
 
-echo "$(date) - Installing Speedtest Mod..."
+if [ ! -f /usr/local/bin/pihole ]; then
+	echo "$(date) - Installing Pi-hole..."
+	curl -sSLN https://install.pi-hole.net | sudo bash
+fi
+
+echo "$(date) - Verifying dependencies..."
+
+PHP_VERSION=$(php -v | tac | tail -n 1 | cut -d " " -f 2 | cut -c 1-3)
+apt-get install sqlite3 $PHP_VERSION-sqlite3 jq -y
+apt-get remove speedtest-cli -y
+
+if [ ! -f /usr/bin/speedtest ]; then
+	echo "$(date) - Installing speedtest..."
+	# https://www.speedtest.net/apps/cli
+	curl -sSLN https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
+	sudo apt-get install speedtest -y
+fi
+
+echo "$(date) - Downloading Speedtest Mod..."
 
 cd /var/www/html
 rm -rf mod_admin
@@ -17,12 +35,7 @@ cd mod_pihole
 git fetch --tags
 latestTag=$(git describe --tags `git rev-list --tags --max-count=1`)
 git checkout $latestTag
-chmod +x webpage.sh
-
-if [ ! -f /usr/local/bin/pihole ]; then
-	echo "$(date) - Installing Pi-hole..."
-	curl -sSLN https://install.pi-hole.net | sudo bash
-fi
+chmod +x advanced/Scripts/webpage.sh
 
 db=$([ "$1" == "up" ] && echo "$3" || [ "$1" == "un" ] && echo "$2" || echo "$1")
 curl -sSLN https://github.com/arevindh/pihole-speedtest/raw/master/uninstall.sh | sudo bash -s -- $db
@@ -38,22 +51,12 @@ if [ "$1" == "up" ]; then
 	fi
 fi
 
-PHP_VERSION=$(php -v | tac | tail -n 1 | cut -d " " -f 2 | cut -c 1-3)
-apt-get install sqlite3 $PHP_VERSION-sqlite3 jq -y
-apt-get remove speedtest-cli -y
+echo "$(date) - Installing Speedtest Mod..."
 
-if [ ! -f /usr/bin/speedtest ]; then
-	echo "$(date) - Installing speedtest..."
-	# https://www.speedtest.net/apps/cli
-	curl -sSLN https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
-	sudo apt-get install speedtest -y
-fi
-
-echo "$(date) - Modding Pi-hole..."
-
-cd /opt/pihole/
-cp webpage.sh webpage.sh.org
-cp webpage.sh.mod webpage.sh
+cd /opt/
+cp pihole/webpage.sh pihole/webpage.sh.org
+cp mod_pihole/advanced/Scripts/webpage.sh pihole/webpage.sh
+rm -rf mod_pihole
 cd /var/www/html
 rm -rf org_admin
 mv admin org_admin
--- uninstall.sh
+++ uninstall.sh
@@ -9,8 +9,9 @@ if [ ! -f /opt/pihole/webpage.sh.org ]; then
     cd org_pihole
     currVer=$(pihole -v | grep "Pi-hole" | cut -d ' ' -f 3)
     git checkout $currVer >/dev/null 2>&1
+    chmod +x advanced/Scripts/webpage.sh
+    cp advanced/Scripts/webpage.sh ../pihole/webpage.sh.org
     cd -
-    cp org_pihole/webpage.sh pihole/webpage.sh.org
     rm -rf org_pihole
 fi
 

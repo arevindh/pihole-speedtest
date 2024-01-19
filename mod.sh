fa828911babaf7daf65ab6f9e34838b9fd2767f1
ipitio <21136719+ipitio@users.noreply.github.com>
2024-01-19 00:36:17 -0500

    unshallow only if needed

--- mod.sh
+++ mod.sh
@@ -10,45 +10,44 @@ help() {
 }
 
 clone() {
-	local num_args=$#
-	local cd_path=$1
-	local dir_name=$2
-	local repo=$3
-	local repo_name=${4-}
+	local path=$1
+	local dest=$2
+	local src=$3
+	local name=${4-} # if set, will keep local tag if older than latest
 
-	cd "$cd_path"
-	rm -rf "$dir_name"
-	git clone --depth=1 "$repo" "$dir_name"
-	cd "$dir_name"
+	cd "$path"
+	rm -rf "$dest"
+	git clone --depth=1 "$src" "$dest"
+	cd "$dest"
 	git fetch --tags -q
-	latestTag=$(git describe --tags $(git rev-list --tags --max-count=1))
-	if [ "$num_args" -eq 4 ]; then
-		git fetch --unshallow
-		localTag=$(pihole -v | grep "$repo_name" | cut -d ' ' -f 6)
+	local latestTag=$(git describe --tags $(git rev-list --tags --max-count=1))
+	if [ ! -z "$name" ]; then
+		localTag=$(pihole -v | grep "$name" | cut -d ' ' -f 6)
 		if [[ "$localTag" == *.* ]] && [[ "$localTag" < "$latestTag" ]]; then
 			latestTag=$localTag
+			git fetch --unshallow
 		fi
 	fi
 	git -c advice.detachedHead=false checkout $latestTag
 }
 
 download() {
+	echo "$(date) - Installing any missing dependencies..."
+
 	if [ ! -f /usr/local/bin/pihole ]; then
 		echo "$(date) - Installing Pi-hole..."
 		curl -sSL https://install.pi-hole.net | sudo bash
 	fi
 
 	if [ -z "${1-}" ] || [ "$1" == "up" ]; then
-		echo "$(date) - Verifying Dependencies..."
-
 		if [ ! -f /etc/apt/sources.list.d/ookla_speedtest-cli.list ]; then
 			echo "$(date) - Adding speedtest source..."
 			# https://www.speedtest.net/apps/cli
 			if [ -e /etc/os-release ]; then
 				. /etc/os-release
-				base="ubuntu debian"
-				os=${ID}
-				dist=${VERSION_CODENAME}
+				local base="ubuntu debian"
+				local os=${ID}
+				local dist=${VERSION_CODENAME}
 				if [ ! -z "${ID_LIKE-}" ] && [[ "${base//\"/}" =~ "${ID_LIKE//\"/}" ]] && [ "${os}" != "ubuntu" ]; then
 					os=${ID_LIKE%% *}
 					[ -z "${UBUNTU_CODENAME-}" ] && UBUNTU_CODENAME=$(/usr/bin/lsb_release -cs)
@@ -63,7 +62,7 @@ download() {
 				curl -sSLN https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
 			fi
 		fi
-		PHP_VERSION=$(php -v | tac | tail -n 1 | cut -d " " -f 2 | cut -c 1-3)
+		local PHP_VERSION=$(php -v | tac | tail -n 1 | cut -d " " -f 2 | cut -c 1-3)
 		apt-get install -y sqlite3 $PHP_VERSION-sqlite3 jq speedtest-cli- speedtest
 		if [ -f /usr/local/bin/speedtest ]; then
 			rm -f /usr/local/bin/speedtest
@@ -100,13 +99,13 @@ install() {
 	fi
 
 	pihole updatechecker local
-
-	echo "$(date) - Install Complete"
 }
 
 purge() {
+	echo "$(date) - Purging files..."
 	rm -rf /opt/pihole/webpage.sh.*
 	rm -rf /var/www/html/*_admin
+	echo "$(date) - Purge Complete"
 	exit 0
 }
 
@@ -116,14 +115,13 @@ update() {
 	git reset --hard origin/master
 	git checkout master
 	PIHOLE_SKIP_OS_CHECK=true sudo -E pihole -up
-	echo "$(date) - Update Complete"
 	if [ "${1-}" == "un" ]; then
 		purge
 	fi
 }
 
 uninstall() {
-	echo "$(date) - Restoring Pi-hole..."
+	echo "$(date) - Uninstalling Current Speedtest Mod..."
 
 	if [ ! -f /opt/pihole/webpage.sh.org ]; then
 		clone /opt org_pihole https://github.com/pi-hole/pi-hole Pi-hole
@@ -137,11 +135,9 @@ uninstall() {
 	fi
 
 	if [ "${1-}" == "db" ] && [ -f /etc/pihole/speedtest.db ]; then
-		mv /etc/pihole/speedtest.db /etc/pihole/speedtest.db.old
-		echo "$(date) - Configured Database"
+		echo "$(date) - Flushing Database..."
+		mv -f /etc/pihole/speedtest.db /etc/pihole/speedtest.db.old
 	fi
-
-	echo "$(date) - Uninstalling Current Speedtest Mod..."
 
 	cd /var/www/html
 	if [ -d /var/www/html/admin ]; then
@@ -153,8 +149,6 @@ uninstall() {
 	cp webpage.sh webpage.sh.mod
 	mv webpage.sh.org webpage.sh
 	chmod +x webpage.sh
-
-	echo "$(date) - Uninstall Complete"
 }
 
 restore() {
@@ -187,15 +181,17 @@ abort() {
 }
 
 clean() {
+	echo "$(date) - Cleaning up..."
 	rm -rf /var/www/html/mod_admin
 	rm -f /opt/pihole/webpage.sh.mod
 	pihole restartdns
+	echo "$(date) - Clean Complete"
 	exit 0
 }
 
 main() {
 	printf "Thanks for using Speedtest Mod!\nScript by @ipitio\n\n"
-	op=${1-}
+	local op=${1-}
 	if [ "$op" == "-h" ] || [ "$op" == "--help" ]; then
 		help
 		exit 0
@@ -207,7 +203,7 @@ main() {
 	set -Eeuo pipefail
 	trap '[ "$?" -eq "0" ] && clean || abort $op' EXIT
 
-	db=$([ "$op" == "up" ] && echo "${3-}" || [ "$op" == "un" ] && echo "${2-}" || echo "$op")
+	local db=$([ "$op" == "up" ] && echo "${3-}" || [ "$op" == "un" ] && echo "${2-}" || echo "$op")
 	download $op
 	uninstall $db
 	case $op in
@@ -222,6 +218,7 @@ main() {
 		install
 		;;
 	esac
+	echo "$(date) - Process Complete"
 	exit 0
 }
 

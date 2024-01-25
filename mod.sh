b6ab4f3d53d5909177f5bc0d3257638bfb736cf6
ipitio <21136719+ipitio@users.noreply.github.com>
2024-01-25 10:05:39 -0500

    fix fetch

--- mod.sh
+++ mod.sh
@@ -12,10 +12,10 @@ help() {
 setTags() {
 	local path=${1-}
 	local name=${2-}
-
 	if [ ! -z "$path" ]; then
 		cd "$path"
-		git fetch --tags -q
+		git fetch origin -q
+		git fetch --tags -f -q
 		latestTag=$(git describe --tags $(git rev-list --tags --max-count=1))
 	fi
 	if [ ! -z "$name" ]; then
@@ -30,7 +30,6 @@ download() {
 	local url=$3
 	local src=${4-}
 	local dest=$path/$name
-
 	if [ ! -d $dest ]; then # replicate
 		cd "$path"
 		rm -rf "$name"
@@ -47,8 +46,12 @@ download() {
 		if [ ! -z "$src" ]; then
 			if [ "$url" != "old" ]; then
 				git config --global --add safe.directory "$dest"
-				git remote -v | grep -q "old" || git remote rename origin old
-				git remote -v | grep -q "origin" && git remote remove origin
+				if ! git remote -v | grep -q "old"; then
+					git remote rename origin old
+				fi
+				if git remote -v | grep -q "origin"; then
+					git remote remove origin
+				fi
 				git remote add origin $url
 			else
 				git remote rename origin new
@@ -206,6 +209,11 @@ uninstall() {
 }
 
 abort() {
+	if (( $aborted == 1 )); then
+		exit 1
+	fi
+	aborted=1
+
 	echo "$(date) - Process Aborting..." | sudo tee -a /var/log/pimod.log
 
 	if [ -f /opt/pihole/webpage.sh.bak ]; then
@@ -252,8 +260,10 @@ main() {
 		sudo "$0" "$@"
 		exit $?
 	fi
+	aborted=0
 	set -Eeuo pipefail
-	trap '[ "$?" -eq "0" ] && commit || abort $op' EXIT
+	trap '[ "$?" -eq "0" ] && commit || abort' EXIT
+	trap 'abort' INT TERM ERR
 
 	local db=$([ "$op" == "up" ] && echo "${3-}" || [ "$op" == "un" ] && echo "${2-}" || echo "$op")
 	case $op in

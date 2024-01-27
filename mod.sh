3105d1396ace75c2bf54f936def39b99f819121a
ipitio <21136719+ipitio@users.noreply.github.com>
2024-01-27 03:57:56 -0500

    finish help

--- mod.sh
+++ mod.sh
@@ -29,6 +29,7 @@ help() {
     echo "up - update Pi-hole (along with the Mod)"
     echo "un - remove the mod (including all backups)"
     echo "db - flush database (restore for a short while after)"
+    echo "If no option is specified, the Mod will be (re)installed."
 }
 
 setTags() {
